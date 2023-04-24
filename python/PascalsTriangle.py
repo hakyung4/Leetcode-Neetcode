4e0de5d12eb0fbedb635872fcb6e163e5aac0647
Hakyung(Peter) Yun <87377304+hakyung4@users.noreply.github.com>
2023-04-24 22:03:08 +0900

    Create PascalsTriangle.py

---
+++ python/PascalsTriangle.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        if numRows == 0:
+            return [[1]]
+        else:
+            return self.getAllRow(numRows - 1)
+
+    def getAllRow(self, numRows):
+        if numRows == 0:
+            return [[1]]
+        ListPrec = self.getAllRow(numRows - 1)
+        Len = len(ListPrec[-1])
+        ListPrec.append([1])
+        for i in range(0, Len - 1):
+            ListPrec[-1].append(ListPrec[-2][i] + ListPrec[-2][i + 1])
+        ListPrec[-1].append(1)
+        return ListPrec
